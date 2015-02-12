a80ba1646aa0ce98a41bc3dba1878105677d5d44
alastair38 <alastair@alastaircox.com>
2015-02-12 13:26:03 +0000

    Updated nav-main-offcanvas.php to include aria support as suggested in http://heydonworks.com/practical_aria_examples/progressive-hamburger.html

--- partials/nav-main-offcanvas.php
+++ partials/nav-main-offcanvas.php
@@ -16,18 +16,18 @@
 <!-- This is the nav that will show for mobile/small devices -->
 <div class="large-12 columns show-for-small-only">
 	<div class="contain-to-grid">
-		<nav class="tab-bar" role="navigation">
+		<nav class="tab-bar">
 			<section class="middle tab-bar-section">
 				<h1 class="title"><?php bloginfo('name'); ?></h1>
 			</section>
 			<section class="left-small">
-				<a href="#" class="left-off-canvas-toggle menu-icon" ><span></span></a>
+				<a href="#" class="left-off-canvas-toggle menu-icon" aria-label="navigation menu" role="button" aria-controls="navigation" aria-expanded="false" ><span></span></a>
 			</section>
 		</nav>
 	</div>
 </div>
 						
-<aside class="left-off-canvas-menu show-for-small-only">
+<aside class="left-off-canvas-menu show-for-small-only" role="navigation">
 	<ul class="off-canvas-list">
 		<li><label>Navigation</label></li>
 			<?php joints_main_nav(); ?>    
